d13ba74b6e786444220cdc02492abbb967d87618
sworc <lightbox@nifty.com>
2022-03-09 14:15:17 +0900

    Add files via upload

---
+++ view.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
+    <meta charset="utf-8">
+    <title>簡易掲示板</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
+<style>
+html,body {
+    height: 100%;
+}
+
+body {
+    margin: 0;
+}
+#head {
+    width: 100%;
+    height: <?= $view_head_height ?>px;
+    background-color: #fff;
+}
+#extend {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: calc( 100% - 3px );
+    height: calc( 100% - <?= $view_head_height ?>px - 2px );
+    border: solid 2px #c0c0c0;
+}
+
+#body {
+    width: 800px;
+    height: 80px;
+}
+</style>
+
+</head>
+
+<body>
+<div id="head">
+    <h3 class="alert alert-primary">掲示板</h3>
+    <div id="content" class="m-4">
+	    <form action="" method="POST" target="myframe">
+	        <div><span style='display:inline-block;width:100px;'>件名</span> <input type="text" name="message" style='width:600px;'> <input type="submit" value="投稿"></div>
+	        <div><span style='display:inline-block;width:100px;'>ユーザ</span> <input type="text" name="user" style='width:600px;'></div>
+	        <div>
+	            <textarea id="body" name="body"></textarea>
+	        </div>
+	    </form>
+    </div>
+</div>
+
+<iframe id="extend" src="init_page.php" name="myframe"></iframe>
+
+</body>
+</html>
