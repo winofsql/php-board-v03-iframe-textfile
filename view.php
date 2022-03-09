af550f486b76b21c279ed4b9531f36f945ba0df0
sworc <lightbox@nifty.com>
2022-03-09 14:18:14 +0900

    Add files via upload

--- view.php
+++ view.php
@@ -3,7 +3,7 @@
 <head>
     <meta content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
     <meta charset="utf-8">
-    <title>簡易掲示板</title>
+    <title>掲示板</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
 <style>
 html,body {
@@ -40,7 +40,7 @@ body {
     <h3 class="alert alert-primary">掲示板</h3>
     <div id="content" class="m-4">
 	    <form action="" method="POST" target="myframe">
-	        <div><span style='display:inline-block;width:100px;'>件名</span> <input type="text" name="message" style='width:600px;'> <input type="submit" value="投稿"></div>
+	        <div><span style='display:inline-block;width:100px;'>件名</span> <input type="text" name="message" style='width:600px;'> <input type="submit" value="投稿" class="ms-3"></div>
 	        <div><span style='display:inline-block;width:100px;'>ユーザ</span> <input type="text" name="user" style='width:600px;'></div>
 	        <div>
 	            <textarea id="body" name="body"></textarea>
--- view2.php
+++ view2.php
@@ -3,7 +3,7 @@
 <head>
     <meta content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
     <meta charset="utf-8">
-    <title>簡易掲示板</title>
+    <title>掲示板</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
 
 <style>
