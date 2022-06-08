5a431603ccad608c09762a9de484af66ce717331
sworc <lightbox@nifty.com>
2022-06-08 15:03:10 +0900

    Add files via upload

--- view.php
+++ view.php
@@ -4,7 +4,8 @@
     <meta content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
-    <title>掲示板</title>
+    <title>掲示板 v03</title>
+    
 <style>
 html,body {
     height: 100%;
